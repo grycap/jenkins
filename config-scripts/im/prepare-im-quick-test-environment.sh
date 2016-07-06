2765cacd2fd4640e1c69ca3cfcaf1cf977432e53
Alfonso Pérez <alpegon3@upvnet.upv.es>
2016-07-06 12:44:56 +0200

    Update prepare-im-quick-test-environment.sh

--- config-scripts/im/prepare-im-quick-test-environment.sh
+++ config-scripts/im/prepare-im-quick-test-environment.sh
@@ -3,14 +3,16 @@
 # $2 -> Container name
 # $3 -> Container image name
 
+GITHUB_URL=https://raw.githubusercontent.com/grycap/jenkins/master/config-scripts/im
+
 # Try to stop any other container with the same name
-curl -s https://raw.githubusercontent.com/grycap-jenkins/config-scripts/master/im/stop-and-remove-docker-containers.sh | bash -s $3
+curl -s $GITHUB_URL/stop-and-remove-docker-containers.sh | bash -s $3
 
 # Build docker image and launch container
-curl -s https://raw.githubusercontent.com/grycap-jenkins/config-scripts/master/im/build-and-launch-docker-container.sh | bash -s $1 $2 $3
+curl -s $GITHUB_URL/build-and-launch-docker-container.sh | bash -s $1 $2 $3
 
 # Disable IPv6 in test machines
-curl -s https://raw.githubusercontent.com/grycap-jenkins/config-scripts/master/im/disable-ipv6-ansible.sh | bash -s $1
+curl -s $GITHUB_URL/disable-ipv6-ansible.sh | bash -s $1
 
 # Configure tests ports
-curl -s https://raw.githubusercontent.com/grycap-jenkins/config-scripts/master/im/configure-ports-quick-tests.sh | bash -s $1 $2
+curl -s $GITHUB_URL/configure-ports-quick-tests.sh | bash -s $1 $2
