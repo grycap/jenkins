ae430231995ef4b529bfb76383d9b8b46bb92399
Alfonso Pérez <alpegon3@upvnet.upv.es>
2016-07-12 10:42:28 +0200

    Add code coverage to clues-indigo tests

--- config-scripts/clues-indigo/executes-tests.sh
+++ config-scripts/clues-indigo/executes-tests.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-nosetests -v tests/indigo_orchestrator_test.py tests/mesos_test.py tests/condor_test.py
+nosetests -v tests/indigo_orchestrator_test.py tests/mesos_test.py tests/condor_test.py --with-xunit --with-coverage --cover-erase --cover-html --cover-xml --cover-package=mesos,indigo_orchestrator,condor
