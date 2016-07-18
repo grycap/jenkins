0581dddadb18c7d75f951b6f041797b7fdf5c5a9
alpegon <alpegon3@upvnet.upv.es>
2016-07-18 16:53:16 +0200

    Update test execution script
    Now the execution will stop if any test fails

--- config-scripts/im/execute-quick-tests.sh
+++ config-scripts/im/execute-quick-tests.sh
@@ -1,2 +1,2 @@
 # Execute tests
-nosetests -v test/unit/connectors/*.py test/unit/*.py test/functional/*.py test/integration/QuickTestIM.py test/integration/TestREST.py --with-timer --timer-no-color --with-xunit --with-coverage --cover-erase --cover-html --cover-package=IM
+nosetests -v test/unit/connectors/*.py test/unit/*.py test/functional/*.py test/integration/QuickTestIM.py test/integration/TestREST.py --stop --with-timer --timer-no-color --with-xunit --with-coverage --cover-erase --cover-html --cover-package=IM
