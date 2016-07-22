9706e5db4dc36b016b4b8d798b6a755c61305631
alpegon <alpegon3@upvnet.upv.es>
2016-07-22 14:23:52 +0200

    Update ec3 scripts
    Add new recipe (centos7)
    Add Jenkinsfile job configuration
    Update testing script

--- config-scripts/ec3/recipes/test.sh
+++ config-scripts/ec3/recipes/test.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 LRMS=$1
-AUTH_FILE=$2
+RECIPE=$2
+AUTH_FILE=$3
 
 # Create EC3 cluster
 echo "Launching cluster"
 cd /opt/ec3
-HOST=$(./ec3 launch myjenkinscluster $LRMS ubuntu14-ramses -a $AUTH_FILE -u http://servproject.i3m.upv.es:8899 -y | grep "IP" | awk 'BEGIN {FS=" "}{print $7}' | awk 'BEGIN {FS="F"}{print $1}' )
+HOST=$(./ec3 launch myjenkinscluster $LRMS $RECIPE -a $AUTH_FILE -u http://servproject.i3m.upv.es:8899 -y | grep "IP" | awk 'BEGIN {FS=" "}{print $7}' | awk 'BEGIN {FS="F"}{print $1}' )
 echo "Cluster successfully created"
 
 if [ "$HOST" == "" ];then
