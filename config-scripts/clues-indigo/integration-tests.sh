7172f558580aea19aa31ca63e27c8b9008e6cd57
Alfonso Pérez <alpegon3@upvnet.upv.es>
2016-07-29 11:46:58 +0200

    Update integration-tests.sh
    Add functions to avoid code repetition
    Fix bad function output

--- config-scripts/clues-indigo/integration-tests.sh
+++ config-scripts/clues-indigo/integration-tests.sh
@@ -38,19 +38,23 @@ echo 'MARATHON TASK SENT'
 sleep 5
 
 function get_marathon_tasks() {
-  echo http GET http://$HOST_IP:8080/v2/apps?embed=tasks -b
+  echo $(http GET http://$HOST_IP:8080/v2/apps?embed=tasks -b)
 }
 
 echo 'CHECKING IF MARATHON RECEIVES THE TASK'
-echo $(get_marathon_tasks) | jq '.apps[0].id' | grep mysql &> /dev/null
+get_marathon_tasks | jq '.apps[0].id' | grep mysql &> /dev/null
 echo 'MARATHON TASK RECEIVED SUCCESFULLY'
 sleep 5
 
+function get_marathon_task_state() {
+  echo $(get_marathon_tasks | jq '.apps[0].tasks[0].state')
+}
+
 echo 'WAITING FOR MARATHON TASK TO DEPLOY'
-TASK_STATE=$($(get_marathon_tasks) | jq '.apps[0].tasks[0].state' | grep -c RUNNING)
+TASK_STATE=$(get_marathon_task_state | grep -c RUNNING)
 while [[ $TASK_STATE -ne 1 ]]; do
   sleep 20
-  TASK_STATE=$($(get_marathon_tasks) | jq '.apps[0].tasks[0].state' | grep -c RUNNING)
+  TASK_STATE=$(get_marathon_task_state | grep -c RUNNING)
   echo 'WAITING FOR MARATHON TASK TO DEPLOY'
 done
 echo 'MARATHON TASK RUNNING'
