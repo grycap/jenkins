936f716b60222cb069c4f6c0cb1f31bf51c4526e
alpegon <alpegon3@upvnet.upv.es>
2016-08-05 13:54:07 +0200

    Fix script error

--- config-scripts/im/build-and-launch-im-container.sh
+++ config-scripts/im/build-and-launch-im-container.sh
@@ -6,10 +6,10 @@
 cd $1/docker-devel
 
 # Create image based on 'devel' branch
-docker build -t $2 --no-cache -f Dockerfile .
+docker build -t $3 --no-cache -f Dockerfile .
 
 # Start container
-docker run -d -P --name $3 $2
+docker run -d -P --name $2 $3
 
 # Wait for the container
 sleep 10
