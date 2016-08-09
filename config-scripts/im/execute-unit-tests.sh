c39d2a8e567e4f3af6e248cc34d5b7292818a685
alpegon <alpegon3@upvnet.upv.es>
2016-08-09 11:27:21 +0200

    Update im unit test script

--- config-scripts/im/execute-unit-tests.sh
+++ config-scripts/im/execute-unit-tests.sh
@@ -2,10 +2,4 @@
 WORKSPACE=$1
 
 cd $WORKSPACE
-TESTS="test/unit/connectors/*.py test/unit/*.py test/functional/*.py"
-BASIC_PARAM="-v --stop"
-XUNIT="--with-xunit"
-TIMER="--with-timer --timer-no-color"
-COVERAGE="--with-coverage --cover-erase --cover-html --cover-package=IM"
-
-nosetests $TESTS $BASIC_PARAM $XUNIT $TIMER $COVERAGE
+nosetests test/unit/connectors/*.py test/unit/*.py test/functional/*.py -v --stop --with-xunit --with-timer --timer-no-color --with-coverage --cover-erase --cover-html --cover-package=IM
