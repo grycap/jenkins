53b55efa31aa276d8e24008494c8bc6fa7ea010d
alpegon <alpegon3@upvnet.upv.es>
2016-08-09 13:51:04 +0200

    Fix ports script

--- config-scripts/im/configure-ports.sh
+++ config-scripts/im/configure-ports.sh
@@ -7,13 +7,13 @@ NAME_ID=$2
 cd $WORKSPACE/test
 
 # Get running port
-XMLRPC_PORT=$(docker port $NAME_ID 8899 | awk -F "[:]" '{print $2}')
+XMLRPC_PORT=$(sudo docker port $NAME_ID 8899 | awk -F "[:]" '{print $2}')
 # Set running port
 sed -i "s/TEST_PORT = 8899/TEST_PORT = $XMLRPC_PORT/g" integration/QuickTestIM.py
 sed -i "s/TEST_PORT = 8899/TEST_PORT = $XMLRPC_PORT/g" integration/TestIM.py
 
 # Get running port
-REST_PORT=$(docker port $NAME_ID 8800 | awk -F "[:]" '{print $2}')
+REST_PORT=$(sudo docker port $NAME_ID 8800 | awk -F "[:]" '{print $2}')
 # Set running port
 sed -i "s/TEST_PORT = 8800/TEST_PORT = $REST_PORT/g" integration/TestREST.py
 sed -i "s/TEST_PORT = 8800/TEST_PORT = $REST_PORT/g" integration/TestREST_JSON.py
