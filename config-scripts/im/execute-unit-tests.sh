86a6967f7d6c1ffa656be9616aa52b59fe20a5cd
alpegon <alpegon3@upvnet.upv.es>
2016-08-09 13:21:29 +0200

    Update im unit test script
    Add coverage xml report generation to the script

--- config-scripts/im/execute-unit-tests.sh
+++ config-scripts/im/execute-unit-tests.sh
@@ -2,4 +2,4 @@
 WORKSPACE=$1
 
 cd $WORKSPACE
-nosetests test/unit/connectors/*.py test/unit/*.py test/functional/*.py -v --stop --with-xunit --with-timer --timer-no-color --with-coverage --cover-erase --cover-html --cover-package=IM
+nosetests test/unit/connectors/*.py test/unit/*.py test/functional/*.py -v --stop --with-xunit --with-timer --timer-no-color --with-coverage --cover-erase --cover-html --cover-xml --cover-package=IM
