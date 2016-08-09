e62b808e86880492674f0a33d4a294d81d594f13
alpegon <alpegon3@upvnet.upv.es>
2016-08-09 11:21:49 +0200

    Fix test parameters

--- config-scripts/im/execute-unit-tests.sh
+++ config-scripts/im/execute-unit-tests.sh
@@ -2,10 +2,10 @@
 WORKSPACE=$1
 
 cd $WORKSPACE
-TESTS=test/unit/connectors/*.py test/unit/*.py test/functional/*.py
-BASIC_PARAM=-v --stop
-XUNIT=--with-xunit
-TIMER=--with-timer --timer-no-color
-COVERAGE=--with-coverage --cover-erase --cover-html --cover-package=IM
+TESTS="test/unit/connectors/*.py test/unit/*.py test/functional/*.py"
+BASIC_PARAM="-v --stop"
+XUNIT="--with-xunit"
+TIMER="--with-timer --timer-no-color"
+COVERAGE="--with-coverage --cover-erase --cover-html --cover-package=IM"
 
 nosetests $TESTS $BASIC_PARAM $XUNIT $TIMER $COVERAGE
