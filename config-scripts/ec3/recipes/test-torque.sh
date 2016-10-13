ad94b0b2700bf3491d077afd34c2ed9757967668
alpegon <alpegon3@upvnet.upv.es>
2016-10-13 16:08:00 +0200

    Add torque tests for ec3

---
+++ config-scripts/ec3/recipes/test-torque.sh
@@ -0,0 +1,25 @@
+#!/bin/bash -e
+pbsnodes -a
+cat >> test.sh << EOF
+#!/bin/bash
+date > date.out
+/bin/hostname
+EOF
+chmod +x test.sh
+sleep 2
+
+echo "Start submitting jobs"
+for i in 1 2 3
+do
+    qsub test.sh > job$i.out
+    echo "Job $i finished"
+    sleep 2
+done
+
+for i in 1 2 3
+do
+    echo "Job $i output"
+    cat job$i.out
+done
+
+exit
