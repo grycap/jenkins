94389a8fa1b1f1b7ac7d56b62a9203749c8cb250
micafer <micafer1@upv.es>
2017-03-01 09:08:40 +0100

    Add im-py3 tests

---
+++ config-scripts/im/execute-unit-py3-tests.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+WORKSPACE=$1
+
+cd $WORKSPACE
+nosetests3 test/unit/connectors/*.py test/unit/*.py test/functional/*.py -v --stop --with-xunit --with-timer --timer-no-color --with-coverage --cover-erase --cover-xml --cover-package=IM
