b5185bd0da6d8a9d8757e85fe4e5a8ba3e143221
Miguel Caballer <micafer1@upv.es>
2017-03-02 16:10:14 +0100

    add contextualization package

--- config-scripts/im/execute-unit-py3-tests.sh
+++ config-scripts/im/execute-unit-py3-tests.sh
@@ -2,4 +2,4 @@
 WORKSPACE=$1
 
 cd $WORKSPACE
-nosetests3 test/unit/connectors/*.py test/unit/*.py test/functional/*.py -v --stop --with-xunit --with-coverage --cover-erase --cover-xml --cover-package=IM
+nosetests3 test/unit/connectors/*.py test/unit/*.py test/functional/*.py -v --stop --with-xunit --with-coverage --cover-erase --cover-xml --cover-package=IM,contextualization
