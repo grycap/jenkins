af64bc8bb36677011c8cd5be74d394c6119340b9
alpegon <alpegon3@upvnet.upv.es>
2017-04-12 18:02:46 +0200

    Update onedock tests

--- config-scripts/onedock/create-lxc-container.sh
+++ config-scripts/onedock/create-lxc-container.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
+CREATION_SCRIPT=$1
+NAME=$2
+
 # First create the container
 cd install/lxc/
 
-sed -i 's/sleep 5/sleep 10/g' create-lxc
+sed -i 's/sleep 5/sleep 10/g' $CREATION_SCRIPT
 
-sudo ./create-lxc onetest --create
+sudo ./$CREATION_SCRIPT $NAME --create
 
 # Give oneadmin sudo rights
-sudo lxc-attach -n onetest -- sudo adduser oneadmin sudo
-sudo lxc-attach -n onetest -- sed -i.bkp -e 's/%sudo\s\+ALL=(ALL\(:ALL\)\?)\s\+ALL/%sudo ALL=NOPASSWD:ALL/g' /etc/sudoers
+sudo lxc-attach -n $NAME -- sudo adduser oneadmin sudo
+sudo lxc-attach -n $NAME -- sed -i.bkp -e 's/%sudo\s\+ALL=(ALL\(:ALL\)\?)\s\+ALL/%sudo ALL=NOPASSWD:ALL/g' /etc/sudoers
--- config-scripts/onedock/stop-destroy-existing-containers.sh
+++ config-scripts/onedock/stop-destroy-existing-containers.sh
@@ -1,11 +1,10 @@
 #!/bin/sh -x
 # Try to stop any other container with the same name
-if sudo lxc-ls -f | grep onetest
+if sudo lxc-ls -f | grep $1
 then
 	if sudo lxc-ls -f | grep RUNNING
     then
-    	sudo lxc-stop -n onetest
+    	sudo lxc-stop -n $1
     fi
-    sudo lxc-destroy -n onetest
+    sudo lxc-destroy -n $1
 fi
-
