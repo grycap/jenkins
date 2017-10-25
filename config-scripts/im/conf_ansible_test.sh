4193ff7282981d8ee12450afe9dcd54244008c02
Miguel Caballer <micafer1@upv.es>
2017-10-25 08:29:01 +0200

    Add PORT parameter

--- config-scripts/im/conf_ansible_test.sh
+++ config-scripts/im/conf_ansible_test.sh
@@ -3,10 +3,11 @@
 # $1 -> Docker image of the O.S. to test
 
 SO=$1
+PORT=$2
 
 cat <<EOT > inv
 [all]
-localhost ansible_port=1022 ansible_user=root ansible_ssh_pass=Tututu+01
+localhost ansible_port=$PORT ansible_user=root ansible_ssh_pass=Tututu+01
 EOT
 
 cat <<EOT > ansible.cfg
@@ -19,8 +20,8 @@ EOT
 
 rm -f $HOME/.ssh/known_hosts
 
-echo "Launch container for SO: $SO"
-CONT_ID=$(docker run -p 1022:22 -d $SO /bin/bash -c "zypper -n install which sudo openssh; yum install -y sudo openssh-server python ; apt-get update && apt-get install -y sudo openssh-server python ; mkdir /var/run/sshd ; sed -i 's/PermitRootLogin without-password/PermitRootLogin yes/g' /etc/ssh/sshd_config ; sed -i 's/PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config ; sed -i 's/#PermitRootLogin yes/PermitRootLogin yes/g' /etc/ssh/sshd_config; rm -f /etc/ssh/ssh_host_rsa_key* ; ssh-keygen -t rsa -f /etc/ssh/ssh_host_rsa_key -N '' ; echo 'root:Tututu+01' | chpasswd ; sed 's@session\s*required\s*pam_loginuid.so@session optional pam_loginuid.so@g' -i /etc/pam.d/sshd ;  echo 'RUNNING'; /usr/sbin/sshd -D")
+echo "Launch container for SOf: $SO"
+CONT_ID=$(docker run -p $PORT:22 -d $SO /bin/bash -c "zypper -n install which sudo openssh; yum install -y sudo openssh-server python ; apt-get update && apt-get install -y sudo openssh-server python ; mkdir /var/run/sshd ; sed -i 's/PermitRootLogin without-password/PermitRootLogin yes/g' /etc/ssh/sshd_config ; sed -i 's/PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config ; sed -i 's/#PermitRootLogin yes/PermitRootLogin yes/g' /etc/ssh/sshd_config; rm -f /etc/ssh/ssh_host_rsa_key* ; ssh-keygen -t rsa -f /etc/ssh/ssh_host_rsa_key -N '' ; echo 'root:Tututu+01' | chpasswd ; sed 's@session\s*required\s*pam_loginuid.so@session optional pam_loginuid.so@g' -i /etc/pam.d/sshd ;  echo 'RUNNING'; /usr/sbin/sshd -D")
 
 OPEN="0"
 while [ $OPEN -eq "0" ]; do
