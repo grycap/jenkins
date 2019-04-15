a8463a4b847f0ef66fdd978a9624d1bbdcb375a6
Miguel Caballer <micafer1@upv.es>
2019-04-15 12:46:36 +0200

    Set different name

--- config-scripts/im/conf_ansible_test.sh
+++ config-scripts/im/conf_ansible_test.sh
@@ -3,9 +3,10 @@
 # $1 -> Docker image of the O.S. to test
 
 SO=$1
+CONT_NAME=$(echo "confansible_$1" | tr ':' '_')
 
 echo "Launch container for SOf: $SO"
-CONT_ID=$(docker run --name confansible -d $SO /bin/bash -c "zypper -n install which sudo openssh; yum install -y sudo openssh-server python ; apt-get update && apt-get install -y sudo openssh-server python ; mkdir /var/run/sshd ; sed -i 's/PermitRootLogin without-password/PermitRootLogin yes/g' /etc/ssh/sshd_config ; sed -i 's/PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config ; sed -i 's/#PermitRootLogin yes/PermitRootLogin yes/g' /etc/ssh/sshd_config; rm -f /etc/ssh/ssh_host_rsa_key* ; ssh-keygen -t rsa -f /etc/ssh/ssh_host_rsa_key -N '' ; echo 'root:Tututu+01' | chpasswd ; sed 's@session\s*required\s*pam_loginuid.so@session optional pam_loginuid.so@g' -i /etc/pam.d/sshd ;  echo 'RUNNING'; /usr/sbin/sshd -D")
+CONT_ID=$(docker run --name $CONT_NAME -d $SO /bin/bash -c "zypper -n install which sudo openssh; yum install -y sudo openssh-server python ; apt-get update && apt-get install -y sudo openssh-server python ; mkdir /var/run/sshd ; sed -i 's/PermitRootLogin without-password/PermitRootLogin yes/g' /etc/ssh/sshd_config ; sed -i 's/PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config ; sed -i 's/#PermitRootLogin yes/PermitRootLogin yes/g' /etc/ssh/sshd_config; rm -f /etc/ssh/ssh_host_rsa_key* ; ssh-keygen -t rsa -f /etc/ssh/ssh_host_rsa_key -N '' ; echo 'root:Tututu+01' | chpasswd ; sed 's@session\s*required\s*pam_loginuid.so@session optional pam_loginuid.so@g' -i /etc/pam.d/sshd ;  echo 'RUNNING'; /usr/sbin/sshd -D")
 
 OPEN="0"
 while [ $OPEN -eq "0" ]; do
@@ -35,7 +36,7 @@ RES=$?
 if [ $RES -eq 0 ]; then
     echo "First attempt finished successfully"
     # Test a reconfigure
-    docker run --rm --link confansible:confansible -v "$PWD/inv:/tmp/inv" -v "$PWD/conf-ansible.yml:/tmp/conf-ansible.yml" -i grycap/im ansible-playbook -i /tmp/inv /tmp/conf-ansible.yml -e IM_HOST=confansible
+    docker run --rm --link $CONT_ID:confansible -v "$PWD/inv:/tmp/inv" -v "$PWD/conf-ansible.yml:/tmp/conf-ansible.yml" -i grycap/im ansible-playbook -i /tmp/inv /tmp/conf-ansible.yml -e IM_HOST=confansible
     RES=$?
     if [ $RES -eq 0 ]; then
         echo "Reconfiguration finished successfully"
