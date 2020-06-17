0291437fc835cf18468784a8594aa88d6f71083e
Miguel Caballer <micafer1@upv.es>
2020-06-17 10:19:41 +0200

    Update check-codestyle.sh

--- config-scripts/im/check-codestyle.sh
+++ config-scripts/im/check-codestyle.sh
@@ -4,4 +4,4 @@ WORKSPACE=$1
 
 cd $WORKSPACE
 
-pycodestyle --max-line-length=120 --ignore=E402,W504,W605 . --exclude=doc
+pycodestyle --max-line-length=120 --ignore=E402,W504,W605,E722 . --exclude=doc
