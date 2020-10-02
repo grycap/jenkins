f59b837949cb298b119262b239433831ec7be666
Miguel Caballer <micafer1@upv.es>
2020-10-02 08:22:18 +0200

    Update execute-integration-tests.sh

--- config-scripts/im/execute-integration-tests.sh
+++ config-scripts/im/execute-integration-tests.sh
@@ -2,4 +2,4 @@
 WORKSPACE=$1
 
 cd $WORKSPACE
-nosetests-2.7 test/integration/TestREST_JSON.py test/integration/TestREST.py test/integration/TestIM.py -v --stop --with-xunit --with-timer --timer-no-color --with-coverage --cover-erase --cover-xml --cover-package=IM
+nosetests test/integration/TestREST_JSON.py test/integration/TestREST.py test/integration/TestIM.py -v --stop --with-xunit --with-timer --timer-no-color --with-coverage --cover-erase --cover-xml --cover-package=IM
