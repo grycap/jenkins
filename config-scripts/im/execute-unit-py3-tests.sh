19c8ffbbd4b732dac345abab5c6913a30c4cceea
Miguel Caballer <micafer1@upv.es>
2020-10-02 12:07:10 +0200

    Update execute-unit-py3-tests.sh

--- config-scripts/im/execute-unit-py3-tests.sh
+++ config-scripts/im/execute-unit-py3-tests.sh
@@ -2,4 +2,8 @@
 WORKSPACE=$1
 
 cd $WORKSPACE
-nosetests3 test/unit/connectors/*.py test/unit/*.py test/functional/*.py -v --stop --with-xunit --with-coverage --cover-erase --cover-xml --cover-package=IM,contextualization
+if [ -f "/usr/bin/nosetests3" ]; then
+  nosetests3 test/unit/connectors/*.py test/unit/*.py test/functional/*.py -v --stop --with-xunit --with-coverage --cover-erase --cover-xml --cover-package=IM,contextualization
+ else
+  nosetests test/unit/connectors/*.py test/unit/*.py test/functional/*.py -v --stop --with-xunit --with-coverage --cover-erase --cover-xml --cover-package=IM,contextualization
+ fi
