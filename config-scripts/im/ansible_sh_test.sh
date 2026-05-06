8a52cf7b7cb3db7a7e077fbef32fb587a2fbb5c4
Miguel Caballer Fernandez <micafer1@upv.es>
2026-05-06 09:51:28 +0200

    Modify ansible_sh_test.sh to use branch variable
    Updated the script to use a branch variable for fetching the ansible_install.sh script and changed the command to check the ansible version.

--- config-scripts/im/ansible_sh_test.sh
+++ config-scripts/im/ansible_sh_test.sh
@@ -4,8 +4,9 @@
 
 SO=$1
 CONT_NAME=$(echo "sh_ansible_$1" | tr ':' '_' | tr '/' '_')
+BRANCH=${2:-devel}
 
-curl -s https://raw.githubusercontent.com/grycap/im/devel/contextualization/ansible_install.sh > ansible_install.sh
+curl -s https://raw.githubusercontent.com/grycap/im/$BRANCH/contextualization/ansible_install.sh > ansible_install.sh
 
 echo "Launch container for SO: $SO"
 CONT_ID=$(docker run --name $CONT_NAME -v "$PWD/ansible_install.sh:/tmp/ansible_install.sh" -d $SO /bin/bash -c "zypper -n install which sudo; yum install -y sudo;  apt-get update && apt-get install -y sudo ; echo 'RUNNING'; sleep 99999999")
@@ -21,7 +22,7 @@ docker exec -ti $CONT_ID /bin/bash -c "chmod +x /tmp/ansible_install.sh"
 
 docker exec -ti $CONT_ID /bin/bash -c "/tmp/ansible_install.sh /tmp/test.log"
 
-docker exec -t $CONT_ID ansible --version
+docker exec -t $CONT_ID /var/tmp/.mamba/bin/micromamba run -n ansible ansible --version
 
 echo "Removing container $CONT_ID"
 docker rm -f $CONT_ID
